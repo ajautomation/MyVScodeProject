130f0d177601bcdb64124ac7b5fc8d79f0499d98
AjAutomation <AjAutomation09@gmail.com>
2023-07-26 14:05:35 +0530

    My First Commit

---
+++ src/test/java/tests/First_Test.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class First_Test {
+
+	@Test
+	public void m1() {
+		System.out.println("TestCase has run successfully !!");
+
+
+	}
+}
